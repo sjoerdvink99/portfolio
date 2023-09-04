0884cd4034a943d8a2452412ad9aef2a82caaa5f
2427021 <s.a.vink@students.uu.nl>
2023-09-04 21:21:57 +0200

    education and work

--- src/Education.js
+++ src/Education.js
@@ -4,14 +4,16 @@ export default function Education() {
   return (
     <div className="mb-4">
       <h2 className="font-semibold mb-2">Education</h2>
+      <div className="border-b-2 border-indigo-500 mb-2"></div>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 sm:gap-0">
-        <div className="border p-5 rounded-sm shadow mb-4">
+        <div className="border p-5 rounded-sm shadow">
           <h3 className="font-semibold">MSc Applied Data-Science</h3>
           <p className="text-slate-400 text-sm font-light">
             Utrecht University (2023 - 2024)
           </p>
+          <p className="text-sm font-light">Starting in september</p>
         </div>
-        <div className="border p-5 rounded-sm shadow mb-4">
+        <div className="border p-5 rounded-sm shadow">
           <h3 className="font-semibold">BSc Information Science</h3>
           <p className="text-slate-400 text-sm font-light">
             Utrecht University (2020 - 2023)
@@ -20,11 +22,12 @@ export default function Education() {
             Exchange: University of Wisconsin-Madison (Fall 2022)
           </p>
         </div>
-        <div className="border p-5 rounded-sm shadow mb-4">
+        <div className="border p-5 rounded-sm shadow">
           <h3 className="font-semibold">BA Sports management</h3>
           <p className="text-slate-400 text-sm font-light">
             Windesheim University (2016 - 2020)
           </p>
+          <p className="text-sm font-light">Minor in Marketing Management</p>
         </div>
       </div>
     </div>
