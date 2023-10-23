5e274e7259751416aaa7af0f2040984f0a823698
marieverdonck9 <marieverdonck9@gmail.com>
2023-10-23 12:06:42 +0200

    fixed link

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -74,7 +74,7 @@ export const projectInfo = [
   {
     name: "Full text-based search engine",
     date: "June 2021",
-    link: "hhttps://github.com/sjoerdvink99/python-searchengine",
+    link: "https://github.com/sjoerdvink99/python-searchengine",
     skills: ["Python", "Pandas", "SK-Learn"],
     description:
       "A Python search-engine without the use of external libraries to calculate cosinus similarities between queries and documents. The code is primarily written in Dutch, but can be used for other languages. The most important thing is that the documents that you want to search through are in a .txt file. Flask is used for the GUI.",
