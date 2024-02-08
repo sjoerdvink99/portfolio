ad7edcafbc85f1aa923b3548cdd1b5235038c533
2427021 <s.a.vink@students.uu.nl>
2024-02-08 20:59:19 +0100

    updated company description

--- src/Experience.js
+++ src/Experience.js
@@ -19,7 +19,10 @@ export default function Experience() {
           </p>
         </div>
         <p className="text-sm font-light">
-          Thesis on Automated Design of Network Visualizations.
+          At Tufts University's Visual Analytics Lab, I will research automated
+          design for network visualizations, aiming to improve data
+          interpretation and user interaction. This opportunity offers an
+          exciting exploration into the dynamic realm of visual analytics.
         </p>
       </div>
       <div className="border p-5 rounded-sm mb-4">
@@ -38,7 +41,11 @@ export default function Experience() {
           </p>
         </div>
         <p className="text-sm font-light">
-          Building the future of graph analytics and visualisation.
+          In my software engineering role at GraphPolaris, I contribute to
+          enabling the valorization of knowledge stored in graph databases. By
+          empowering users without a dedicated data science background, we make
+          complex analytical tasks on graphs accessible, explainable, and
+          trustworthy.
         </p>
       </div>
       <div className="border p-5 rounded-sm mb-4">
@@ -55,8 +62,8 @@ export default function Experience() {
           <p className="text-slate-400 text-sm font-light">Spring 2023</p>
         </div>
         <p className="text-sm font-light">
-          Assistant in the Web Technology course, covering methods and
-          techniques for both front-end and back-end development of
+          Assistant in the Web Technology course during my bachelor. It covered
+          methods and techniques for both front-end and back-end development of
           web-applications.
         </p>
       </div>
@@ -75,7 +82,8 @@ export default function Experience() {
         </div>
         <p className="text-sm font-light">
           Developed responsive and user-friendly websites tailored to the unique
-          needs of local businesses.
+          needs of local businesses, while providing maintenance and support
+          services guaranteeing optimal performance and functionality.
         </p>
       </div>
     </div>
