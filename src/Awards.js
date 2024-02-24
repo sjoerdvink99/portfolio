426f1d9348589c349425f54f28b48166d654de9f
2427021 <s.a.vink@students.uu.nl>
2024-02-24 18:17:07 +0100

    added achievement

--- src/Awards.js
+++ src/Awards.js
@@ -17,6 +17,13 @@ export default function Awards() {
           sustainable energy park.
         </p>
       </div>
+      <div>
+        <p>
+          <span className="font-bold">CodeX publication (2022):</span> Published
+          2 times in the tech-oriented Medium blog with over 900k monthly
+          readers.
+        </p>
+      </div>
     </div>
   );
 }
